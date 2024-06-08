6a42eca005e88808c0349864e250b93e02df0d2c
artem <artem-belgorodkiy@yandex.ru>
2024-06-08 15:24:47 +0300

    готово

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,143 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import React, { useState, useEffect } from 'react';
+import { Bar } from 'react-chartjs-2';import {CategoryScale} from 'chart.js';
+import Chart from 'chart.js/auto';
+import {items} from "./data";
+import {items2} from "./data";
+import _ from "underscore";
+
+Chart.register(CategoryScale);
+
+
+export const options = {
+  indexAxis: 'y' as const,
+  elements: {
+    bar: {
+      borderWidth: 2,
+    },
+  },
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'right' as const,
+    },
+    title: {
+      display: true,
+      text: 'Гравик сравнения характеристик облачных серверов',
+    },
+  },
+};
+
+
+
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  
+  const [item1, setItem1] = useState(items[0]);
+  const [item2, setItem2] = useState(items[0]);
+  const [item3, setItem3] = useState(items2[0]);
+  const [item4, setItem4] = useState(items2[0]);
+  
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+  
+  let arr1 = [];
+  let arr2 = [];
+  let arr3 = [];
+  let arr4 = [];
+ 
+  arr1.push(_.omit(item1, 'name'));
+  arr2.push(_.omit(item2, 'name'));
+  arr3.push(_.omit(item3, 'name'));
+  arr4.push(_.omit(item4, 'name'));
+  
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+  function set1 () {
+    
+    setItem1(items[event?.target.value])
+    setItem3(items2[event?.target.value])
+  
+  }
+  function set2 () {
+    
+    setItem2(items[event?.target.value])
+    setItem4(items2[event?.target.value])
+  }
+
+  let labels: string[] = _.keys(arr1[0]);
+  let labels2: string[] = _.keys(arr3[0]);
+
+  let data = {
+    labels,
+    datasets: [
+      {
+        label: item1.name,
+        data: _.values(arr1[0]),
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: item2.name,
+        data: _.values(arr2[0]),
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
+
+  let data2 = {
+    labels: labels2,
+    datasets: [
+      {
+        label: item3.name,
+        data: _.values(arr3[0]),
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: item4.name,
+        data: _.values(arr4[0]),
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
+
+  console.log(items2[0])
+
+ 
+  return (<>
+  <h4 className='title'>Выберите компании для сравнения</h4>
+  <div className='selectdiv'>
+<select  onChange={set1}>
+         <option value={0}>GCE</option>
+         <option value={1}>AWS</option>
+         <option value={2}>Azure</option>
+         <option value={3}>Яндекс облако</option>
+         <option value={4}>Майл.ру облако</option>
+         <option value={5}>Selectel</option>
+         <option value={6}>VPC</option>
+      </select>
+  </div>
+   <div className='selectdiv'>
+<select onChange={set2}>
+         <option value={0}>GCE</option>
+         <option value={1}>AWS</option>
+         <option value={2}>Azure</option>
+         <option value={3}>Яндекс облако</option>
+         <option value={4}>Майл.ру облако</option>
+         <option value={5}>Selectel</option>
+         <option value={6}>VPC</option>
+      </select>
+   </div>
+      
+
+  <div style={{ width: '1200px', height: '1200px' }}>
+    <Bar options={options} data={data}/>
+    <Bar options={options} data={data2}/>
+  </div>
+  </>
   );
 }
